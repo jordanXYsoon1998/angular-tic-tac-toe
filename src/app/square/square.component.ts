da7d5eb165961083ca69adb990a51778f813c1a0
Jordan Soon <jordansoon@ucla.edu>
2020-05-25 04:31:36 -0700

    Changed single square to interface

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { SingleSquare } from '../single-square';
 
 @Component({
   selector: 'app-square',
@@ -8,19 +9,20 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SquareComponent implements OnInit {
   // Single square in the 3x3 grid
   arrOfValues: string[] = ['', 'x', 'o'];
-  currNum: number;
-  content: string;
+  square: SingleSquare;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.currNum = 0;
-    this.content = this.arrOfValues[this.currNum];
+    this.square = {
+      index: 0,
+      content: ''
+    };
   }
 
   cycleContent(): void {
-    this.currNum = (this.currNum + 1) % 3;
-    this.content = this.arrOfValues[this.currNum];
+    this.square.index = (this.square.index + 1) % 3;
+    this.square.content = this.arrOfValues[this.square.index];
   }
 
 }

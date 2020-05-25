23425979cbab3359e1d8e0aee767ea4dce001ef3
Jordan Soon <jordansoon@ucla.edu>
2020-05-25 04:22:16 -0700

    Single square clicking works

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -7,11 +7,20 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SquareComponent implements OnInit {
   // Single square in the 3x3 grid
-  @Input() content: string;
+  arrOfValues: string[] = ['', 'x', 'o'];
+  currNum: number;
+  content: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.currNum = 0;
+    this.content = this.arrOfValues[this.currNum];
+  }
+
+  cycleContent(): void {
+    this.currNum = (this.currNum + 1) % 3;
+    this.content = this.arrOfValues[this.currNum];
   }
 
 }

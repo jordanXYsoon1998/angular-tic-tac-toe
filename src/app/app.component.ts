0fff44b6660f73fa8b6d977b5b4f56881a564c96
Jordan Soon <jordansoon@ucla.edu>
2020-05-25 03:26:36 -0700

    Small change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tic-tac-toe';
+  title = 'Angular Tic Tac Toe';
 }

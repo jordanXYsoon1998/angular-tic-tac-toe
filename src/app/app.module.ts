015ef0fe787d799b1857d9f0c91eb2cd4e7a32a4
Jordan Soon <jordansoon@ucla.edu>
2020-05-25 03:25:13 -0700

    Add components needed to implement TicTacToe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
